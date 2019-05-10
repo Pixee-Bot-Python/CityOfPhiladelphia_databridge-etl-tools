1d04e42696bcef1d85e9187c3bcb4ab14b4ae573
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-10 17:07:33 -0400

    Only cutout etl_read_timestamp if it exists

--- databridge_etl_tools/carto_.py
+++ databridge_etl_tools/carto_.py
@@ -275,8 +275,11 @@ class Carto():
 
     def write(self):
         self.get_csv_from_s3()
-        rows = etl.fromcsv(self.csv_path, encoding='latin-1') \
-                    .cutout('etl_read_timestamp')
+        rows = etl.fromcsv(self.csv_path, encoding='latin-1')
+
+        if 'etl_read_timestamp' in etl.header(rows):
+            rows = rows.cutout('etl_read_timestamp')
+
         header = rows[0]
         str_header = ''
         num_fields = len(header)
