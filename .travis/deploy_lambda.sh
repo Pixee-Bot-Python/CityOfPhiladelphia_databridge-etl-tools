5b07da2586a8f78af81bd06f2f1eeff8a1873649
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-16 15:38:45 -0400

    Use a script to deploy lambda
    Travis lambda deploy doesn't work with s3 deployments

---
+++ .travis/deploy_lambda.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+aws lambda update-function-code  \
+    --function-name databridge-etl-tools \
+    --s3-bucket citygeo-airflow-databridge2 \
+    --s3-key lambda/databridge-etl-tools.zip
