43eccdacae290a18496381ed7a9d6c5af38c1bff
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-16 14:37:39 -0400

    Use TRAVIS_BUILD_DIR env var when building the lambda function

--- .travis/build_lambda.sh
+++ .travis/build_lambda.sh
@@ -5,7 +5,9 @@ set -e
 mkdir dist
 
 echo "Zip up site-packages"
-zip -r dist/databridge-etl-tools.zip /home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/
+cd /home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/
+zip -r $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip .
 
 echo "Zip together previous zip file and lambda function"
-zip -g dist/databridge-etl-tools.zip lambda/function.py
+cd $TRAVIS_BUILD_DIR/lambda
+zip -g $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip function.py
