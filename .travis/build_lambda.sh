1f8cc98467df8f6f6194a7ab59575eff67ee1501
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-20 10:44:23 -0400

    Quiet zip logs

--- .travis/build_lambda.sh
+++ .travis/build_lambda.sh
@@ -8,12 +8,12 @@ mkdir dist
 
 echo "Zip up site-packages, excluding non-compiled psycopg2 files to avoid confusing python imports"
 cd /home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/
-zip -r $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip . -x psycopg2/**\* psycopg2_binary-2.8.2.dist-info/**\
+zip -rq $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip . -x psycopg2/**\* psycopg2_binary-2.8.2.dist-info/**\
 
 echo "Zip together previous zip file and psycopg2 compiled files"
 cd $TRAVIS_BUILD_DIR/
-zip -ur $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip psycopg2-3.6
+zip -urq $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip psycopg2-3.6
 
 echo "Zip together previous zip file and lambda function"
 cd $TRAVIS_BUILD_DIR/lambda
-zip -g $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip index.py
+zip -gq $TRAVIS_BUILD_DIR/dist/databridge-etl-tools.zip index.py
