ef90a1d0481e792fc3e7a4f9d54f7c999864ad5f
Peter Dannemann <Peter.Dannemann@phila.gov>
2019-05-29 15:51:47 -0400

    Try to resolve psycopg2 issue in lambda

--- .travis/build_lambda.sh
+++ .travis/build_lambda.sh
@@ -12,9 +12,12 @@ echo "Zip up site-packages, excluding non-compiled psycopg2 files to avoid confu
 cd /home/travis/virtualenv/python3.5.6/lib/python3.5/site-packages/
 zip -rq $ZIP_FILE_PATH . -x psycopg2/**\* psycopg2_binary-2.8.2.dist-info/**\
 
-echo "Zip together previous zip file and psycopg2 compiled files"
 cd $TRAVIS_BUILD_DIR/
-zip -urq $ZIP_FILE_PATH psycopg2-3.6
+echo "Rename compiled psycopg2 file so python recognizes it"
+mv psycopg2-3.6 psycopg2
+
+echo "Zip together previous zip file and psycopg2 compiled files"
+zip -urq $ZIP_FILE_PATH psycopg2
 
 echo "Zip together previous zip file and lambda function"
 cd $TRAVIS_BUILD_DIR/lambda
--- .travis/get_psycopg2.sh
+++ .travis/get_psycopg2.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-pip install awscli
+cd $TRAVIS_BUILD_DIR
 
 echo "Download compiled psycopg2 files"
 svn export https://github.com/jkehler/awslambda-psycopg2.git/trunk/psycopg2-3.6
