fbe763c32b38d3cf10ba9b6e5b53d1a7c92ce21d
floptical <stephen.macdavid@gmail.com>
2023-01-31 18:34:55 +0000

    remove travis stuff

--- .travis/deploy_lambda.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-aws lambda update-function-code  \
-    --function-name databridge-etl-tools-$ENVIRONMENT \
-    --s3-bucket citygeo-airflow-databridge2 \
-    --s3-key lambda/databridge-etl-tools-$ENVIRONMENT.zip
--- .travis/get_psycopg2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-cd $TRAVIS_BUILD_DIR
-
-echo "Download compiled psycopg2 files"
-svn export https://github.com/jkehler/awslambda-psycopg2.git/trunk/psycopg2-3.6
