ae0c741cb56098544115436d8cd3b23c871e38b8
James Midkiff <james.midkiff@phila.gov>
2023-07-25 19:30:07 +0000

    Update conftest

--- tests/constants.py
+++ tests/constants.py
@@ -6,7 +6,7 @@ POLYGON_JSON_SCHEMA = 'polygon.json'
 POINT_CSV = 'point.csv'
 POLYGON_CSV = 'polygon.csv'
 
-POINT_TABLE_2272_NAME = 'test_point_table_2272'
+POINT_TABLE_2272_NAME = 'test_dbtools_point_2272'
 POINT_TABLE_2272_CSV = 'point_table_2272.csv'
 POINT_TABLE_2272_S3_KEY_CSV = 'staging/citygeo/point_table_2272.csv'
 POINT_TABLE_2272_S3_KEY_JSON = 'schemas/citygeo/point_table_2272.json'
